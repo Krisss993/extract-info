53465ad6c86601b7f19eb2acde59b134de896bc2
Krisss993 <129164886+Krisss993@users.noreply.github.com>
2024-07-16 15:46:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ if review_input:
     )
 
     key_data_extraction = llm.invoke(prompt_with_review)
-    print(type(key_data_extraction))
+    st.write(type(key_data_extraction))
     st.write(key_data_extraction)
